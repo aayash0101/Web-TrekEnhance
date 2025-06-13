c3e2d98d11134c38e89bb6d129cecbb15f467e3b
Aayash <dangaayash@gmail.com>
2025-06-13 19:17:56 +0530

    auth controller done

--- Backend/controller/AuthController.js
+++ Backend/controller/AuthController.js
@@ -14,4 +14,19 @@ exports.signup = async (req, res) => {
   }
 };
 
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password)))
+      return res.status(401).json({ error: 'Invalid credentials' });
 
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1d'
+    });
+
+    res.json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
