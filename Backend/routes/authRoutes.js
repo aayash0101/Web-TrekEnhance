8371a61f346586d32484d431ef58c1af753bcaa0
Aayash <dangaayash@gmail.com>
2025-06-13 19:20:07 +0530

    auth route half

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const { signup, signin } = require('../controller/AuthController');
 
-router.post('/signup', signup);
-router.post('/signin', signin);
+
 
 module.exports = router;
